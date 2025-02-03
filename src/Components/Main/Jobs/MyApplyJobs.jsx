6e484e835323454d4ba63f7504c9957692c04216
Nur Mahammad Mondol Robiul <nurmahammadmondolrobiul@gmail.com>
2025-02-02 23:40:17 -0400

    update some code

--- src/Components/Main/Jobs/MyApplyJobs.jsx
+++ src/Components/Main/Jobs/MyApplyJobs.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../../Provider/useAuth';
 import axios from 'axios';
+import { img } from 'motion/react-client';
 
 const MyApplyJobs = () => {
   const { User } = useAuth();
@@ -10,7 +11,9 @@ const MyApplyJobs = () => {
   useEffect(() => {
     if (User?.email) {
       axios
-        .get(`http://localhost:7000/JobsApply?email=${User.email}`)
+        .get(`http://localhost:7000/JobsApply?email=${User.email}`, {
+          withCredentials: true,
+        })
         .then(res => {
           console.log('Received Jobs:', res.data);
           setMyJob(res.data);
@@ -29,9 +32,10 @@ const MyApplyJobs = () => {
         <table className="table">
           {/* head */}
           <thead className="bg-base-200">
-            <tr className="text-center">
-              <th>No</th>
-              <th>Email</th>
+            <tr className="text-center text-base font-bold">
+              <th>Serial</th>
+              <th>Company Name</th>
+              <th>Department</th>
               <th>Date</th>
               <th>Delete</th>
             </tr>
@@ -40,7 +44,11 @@ const MyApplyJobs = () => {
             {myJob.map((job, i) => (
               <tr className="text-center">
                 <th>{i + 1}</th>
-                <td>{job.ApplicantEmail}</td>
+                <td className="flex justify-center items-center gap-3 font-bold">
+                  <img className="w-10" src={job.Company_Logo} alt="" />
+                  {job.Company}
+                </td>
+                <td>{job.Category}</td>
                 {job.ApplyTime ? <td>{job.ApplyTime}</td> : <td>_ _ _</td>}
 
                 <td>
